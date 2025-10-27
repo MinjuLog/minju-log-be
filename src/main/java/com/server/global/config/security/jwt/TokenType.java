c9390f6d6af5d270c17d50c17d67d6b428b5611c
shimfff <shimseawon1510@gmail.com>
2025-10-27 16:15:30 +0900

    [feature] add token dto

---
+++ src/main/java/com/server/global/config/security/jwt/TokenType.java
@@ -0,0 +1,15 @@
+package com.server.global.config.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TokenType {
+    ACCESS_TOKEN("access_token"),
+    REFRESH_TOKEN("refresh_token")
+    ;
+
+    private final String value;
+}
