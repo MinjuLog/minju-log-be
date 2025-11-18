c8ab9fc135be46a19bdf524217fb5cc6dee1ddb1
shimfff <shimseawon1510@gmail.com>
2025-11-18 10:21:22 +0900

    feat: cors config 작성

--- src/main/java/com/server/global/config/CorsConfig.java
+++ src/main/java/com/server/global/config/CorsConfig.java
@@ -6,4 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")          // 모든 URL 허용
+                .allowedOrigins("*")        // 모든 도메인 허용
+                .allowedMethods("*")        // 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+                .allowedHeaders("*")        // 모든 헤더 허용
+                .allowCredentials(false);   // 크리덴셜 필요 없으면 false, 필요하면 true + allowedOrigins에 "*" 사용 X
+    }
 }
